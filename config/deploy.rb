714e17158b2809de0d668cd3a1336481f4175815
Konstantinos Lamprou <lamprou@live.com>
2015-09-08 16:45:05 +0300

    hola added

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'
+require 'mina/rvm'
+# require 'mina/rbenv'
 
 set :rails_env, 'production'
 
@@ -11,16 +12,19 @@ set :domain,     '31.220.7.207'
 set :user,       'relative'
 set :deploy_to,  "/home/relative/www-apps/mina-sample/#{rails_env}"
 set :app_path,   "#{deploy_to}/#{current_path}"
-set :repository, 'https://github.com/username/mynewapp.git'
+set :repository, 'https://github.com/lampr/rails-sample.git'
 set :branch,     'master'
+
+set :rvm_path,   '/home/relative/.rvm/scripts/rvm'
+set :bundle_bin, '/home/relative/.rvm/wrappers/ruby-2.2.1@global/bundle'
 
 
 task :environment do
-  invoke :'rbenv:load'
+  # invoke :'rvm:load'
+  invole :'rvm:use[ruby-2.2.1]'
 end
 
 task deploy: :environment do
-
   deploy do
     invoke :'git:clone'
     invoke :'bundle:install'
